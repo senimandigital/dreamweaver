33fa42fe77d2f5e49451cad3c91c90c33eac88fc
Eboy <eboy_karya_amatir@yahoo.co.id>
2019-10-19 10:14:55 +0700

    Update auto_update_core_extensions.js

--- auto_update_core_extensions.js
+++ auto_update_core_extensions.js
@@ -1,1 +1,2 @@
-dwscripts.setFileContents( dw.getConfigurationPath() +'workspace/Designer.xml', MMHttp.getText('https://raw.githubusercontent.com/senimandigital/dreamweaver/master/workspace/Designer.xml').data, '', '');
+/* update:2019/10/19 */
+dwscripts.setFileContents( dw.getConfigurationPath() +'/workspace/Designer.xml', MMHttp.getText('https://raw.githubusercontent.com/senimandigital/dreamweaver/master/workspace/Designer.xml').data, '', '');
