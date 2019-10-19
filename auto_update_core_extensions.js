8c889d8732321dc2dccf45f58fbd27cef989254c
Eboy <eboy_karya_amatir@yahoo.co.id>
2019-10-19 10:11:00 +0700

    Script auto update
    Dengan file ini, anda tidak perlu dipusingkan dengan bagaimana mengelola update dreamweaver, semuanya bisa dilakukan secara otomatis hanya dengan menekan satu tombol update.

---
+++ auto_update_core_extensions.js
@@ -0,0 +1,1 @@
+dwscripts.setFileContents( dw.getConfigurationPath() +'workspace/Designer.xml', MMHttp.getText('https://raw.githubusercontent.com/senimandigital/dreamweaver/master/workspace/Designer.xml').data, '', '');
