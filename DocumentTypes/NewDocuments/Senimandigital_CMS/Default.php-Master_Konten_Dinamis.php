064e9588aa613123df1ea376a62ad6336a153607
Eboy <eboy_karya_amatir@yahoo.co.id>
2020-08-10 02:53:23 +0000

    Update Default.php-Master_Konten_Dinamis.php

--- DocumentTypes/NewDocuments/Senimandigital_CMS/Default.php-Master_Konten_Dinamis.php
+++ DocumentTypes/NewDocuments/Senimandigital_CMS/Default.php-Master_Konten_Dinamis.php
@@ -9,14 +9,7 @@
 </section>
 </td>
 <td role="aside">
-<section>
-<h3>Tabel Referensi</h3>
-<ul data-filter="true" active="<?php $_GET['table']; ?>">
-<?php do { // } while ($row_show_tables = mysql_fetch_assoc($show_tables));  ?>
-<li active="="><a href="?referensi[<?php echo $row_show_tables['Tables_in_'. $database_senimandigital]; ?>]"><input url-change="parameter" name="referensi[<?php echo $row_show_tables['Tables_in_'. $database_senimandigital]; ?>]" type="checkbox" value="" /><?php echo $row_show_tables['Tables_in_'. $database_senimandigital]; ?></a></li>
-<?php } while ($row_show_tables = mysql_fetch_assoc($show_tables)); $rows = mysql_num_rows($show_tables); if($rows > 0) { mysql_data_seek($show_tables, 0); $row_show_tables = mysql_fetch_assoc($show_tables); } ?>
-</ul>
-</section>
+<?php include $WEBSITE['HOSTING']['TEMPLATES'] .'php/menu_samping.php'; ?>
 </td>
 </tr>
 </table>
