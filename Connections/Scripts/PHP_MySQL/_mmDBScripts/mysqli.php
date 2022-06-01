785af076749e3356299dfa30f01ca12389aa1671
Eboy <eboy_karya_amatir@yahoo.co.id>
2022-06-01 16:35:18 +0700

    Update mysqli.php

--- Connections/Scripts/PHP_MySQL/_mmDBScripts/mysqli.php
+++ Connections/Scripts/PHP_MySQL/_mmDBScripts/mysqli.php
@@ -1,5 +1,10 @@
-<?php
-$senimandigital = new mysqli("localhost", "senimandigital", "senimandigital", "senimandigital");
+<?php // online update: https://github.com/senimandigital/dreamweaver/blob/master/Connections/Scripts/PHP_MySQL/_mmDBScripts/mysqli.php
+$source = file_get_contents('../Connections/senimandigital.php');
+preg_match('%[$]username_[a-z]+ = "(.*?)";%s', $source, $username); //print_r($username);
+preg_match('%[$]password_[a-z]+ = "(.*?)";%s', $source, $password); //print_r($password);
+preg_match('%[$]database_[a-z]+ = "(.*?)";%s', $source, $database); //print_r($database);
+
+    $senimandigital = new mysqli("localhost", $username[1], $password[1], $database[1]);
 if ($senimandigital -> connect_errno) { echo "Failed to connect to MySQL: " . $senimandigital -> connect_error; exit(); }
 
 if ($_REQUEST['opCode'] == 'getDataDistinct') {
@@ -13,13 +18,11 @@ $output[] = $row_distinct[$_REQUEST['field']];
 
 } else {
 
-$query_describe = "DESCRIBE ". $_REQUEST['table'];
+$query_describe = "DESCRIBE `". $_REQUEST['table'] ."`";
 $describe = $senimandigital->query($query_describe);
 $row_describe = $describe->fetch_assoc();
 $totalRows_describe = $senimandigital->affected_rows;
-do {
-$output[] = $row_describe['Field'] ."|". $row_describe['Null'] ."|". $row_describe['Default'];	
-} while( $row_describe = $describe->fetch_assoc() );
+do { $output[] = $row_describe['Field'] ."|". $row_describe['Null'] ."|". $row_describe['Default']; } while( $row_describe = $describe->fetch_assoc() );
 
 }
 echo implode('|', $output); 
